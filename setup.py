b48685c784eb76e4e6ceaf75826e8c0722de4ecf
Danaé ALBRECHT--MARTIN <danae.albrecht-martin@etu.unistra.fr>
2023-10-20 11:22:09 +0200

    Mise à jour de setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 import os
+import sys
 
 """ -------------------------------------------------------------------------------
 USAGE : 
@@ -67,11 +68,21 @@ def install_dependencies(displayLog):
     if not os.path.exists("logs"):
         os.makedirs("logs")
     
+
     # Open logs file in append mode
     with open(log_file, 'a') as log:
-        for dependency in dependencies:
+        # Initial call to print 0% progress
+        printProgressBar(0, len(dependencies), prefix = 'Progress:', suffix = 'Complete', length = 20)
+        for i, dependency in enumerate(dependencies):
+            start_time = time.time()  # Enregistrer l'heure de début
+            
             # Redirect the output/error to the log file and optionally display it in the console
             result = subprocess.run(["pip", "install", dependency], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            
+            end_time = time.time()  # Enregistrer l'heure de fin
+
+            # Calculer le temps d'installation réel
+            installation_time = end_time - start_time
 
             # Save standard and error output to log file
             log.write(result.stdout)
@@ -80,6 +91,11 @@ def install_dependencies(displayLog):
             if displayLog:
                 print(result.stdout)  # Show standard output in console
                 print(result.stderr)  # Show error output in console
+
+            time.sleep(installation_time)
+            # Update Progress Bar
+            printProgressBar(i + 1, len(dependencies), prefix = 'Progress:', suffix = 'Complete', length = 20)
+
             
     
 def logs_config():
@@ -94,14 +110,36 @@ def logs_config():
         return False
 
 
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r\t\t{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
+
+
 def main():
-    print("\t\t===============================================")
+    print("\n\t\t===============================================")
     print("\t\t           Démarrage de l'installation")
     print("\t\t===============================================\n\n")
     
     time.sleep(1)
     displayLog = logs_config()
-    time.sleep(2)
 
     print("\t\t1. Installation des dépendances en cours...")
     print("\t\t-----------------------------------------------\n")
@@ -109,11 +147,10 @@ def main():
     time.sleep(2)
     install_dependencies(displayLog)
 
-    print("\t\tDépendances installées.\n")
-    time.sleep(1)
+    print("\n\t\tDépendances installées.\n")
     print("\t\tPour plus de détails,\n\t\tveuillez consulter le fichier 'depenencies.log'\n\t\tdisponible dans le dossier 'logs'.\n")
 
-    time.sleep(2)
+    time.sleep(1)
 
     print("\n\t\t===============================================")
     print("\t\t              Fin de l'installation")
