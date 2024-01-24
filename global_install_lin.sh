d2e82548076cae6459812bb88fc76d7dca0fa4dc
Yoann GRIM <yoann.grim@etu.unistra.fr>
2024-01-24 20:16:06 +0100

    Ajout de global_install_lin.sh pour installer python3.9 sur Linux et les dépendances contenu dans requirements.txt

---
+++ global_install_lin.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Mise à jour des paquets et installation de dépendances..."
+
+# Mise à jour des paquets
+sudo apt update
+
+# Installation de software-properties-common pour add-apt-repository
+sudo apt install -y software-properties-common
+
+# Ajout du PPA deadsnakes pour Python
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+
+# Mise à jour des paquets après l'ajout du PPA
+sudo apt update
+
+echo "Installation de Python 3.9.2..."
+# Installation de Python 3.9.2
+sudo apt install -y python3.9
+
+echo "Installation des dépendances avec pip..."
+# Installation des dépendances avec pip
+pip install -r requirements.txt
+
+echo "Script terminé."
